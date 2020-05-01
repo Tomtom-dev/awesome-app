5c30620e6208145c55224186deeaa1e55a887251
Tomtom-dev <thomasheddadji0@gmail.com>
2020-05-01 18:16:44 +0200

    finish update the data

--- src/App.js
+++ src/App.js
@@ -1,32 +1,58 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Title from "./Title";
 import Pokemon from "./pokemon";
+
+const all_pokemon = [
+  {
+    name: "Charizard",
+    weight: 90,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Blaze ", " Solar power", " Tough claws", " Drought"]
+  },
+  {
+    name: "Bulbasaur",
+    weight: 6.9,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Overgrow", " Chlorophyll"]
+  },
+  {
+    name: "Mewtwo",
+    weight: 122,
+    awesome: true,
+    terrifying: true,
+    abilities: ["Pressure", " Unnerve", " Steadfast", " Insomnia"]
+  },
+  {
+    name: "Mega beedrill",
+    weight: 65,
+    awesome: false,
+    terrifying: true,
+    abilities: ["Intimidate", " Unnerve"]
+  }
+];
 
 function App() {
   return (
     <div className="App">
       <main>
-        <Title content="some simple title" />
+        <Title content="Some Simple Title" />
+        {all_pokemon.map((pokemon)=>{
+          return(<Pokemon 
+           name={pokemon.name}
+           weight={pokemon.weight}
+           awesome={pokemon.awesome}
+           terrifying={pokemon.terrifying}
+           abilities={pokemon.abilities}
+          />)
+        })}
+        
       </main>
-      <Pokemon/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save the queen.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello world!
-        </a>
-      </header>
     </div>
   );
 }
+
 
 export default App;
--- src/pokemon.js
+++ src/pokemon.js
@@ -1,55 +1,19 @@
 import React from "react";
 
-export default function Pokemon() {
 
-
-
-  state ={
-     allPokemon : [
-      {
-      name: "Charizard",
-      Weight: 90,
-      Awesome: true,
-      Terrifying: false,
-      Abilities: "Blaze, Solar power, Tough claws, Drought",
-      },
-      {
-      name: "Bulbasaur",
-      Weight: 6.9,
-      Awesome: true,
-      Terrifying: false,
-      Abilities: "	Overgrow, Chlorophyll",
-      },
-        {
-      name: "Mewtwo",
-      Weight: 122,
-      Awesome: true,
-      Terrifying: true,
-      Abilities: "Pressure, Unnerve, Steadfast, Insomnia",
-      },
-        {
-      name: "Charizard",
-      Weight: 65,
-      Awesome: false,
-      Terrifying: true,
-      Abilities: "Intimidate, Unnerve",
-      },
-    ]
-  }
-  
-
-    return (
-      <div>
-       {this.state.allPokemon.map(pokemon => (
-           <Pokemon 
-           name= {pokemon.name}
-        Weight= {pokemon.weight} kg
-        Awesome= {pokemon.Awesome}
-        Terrifying= {pokemon.Terrifying}
-        Abilities= {pokemon.Abilities}/>
-       ))}  
-      </div>
-  )
-  }
-  
-
+export default function Pokemon(props) {
+  return (
+    <div>
+      <h2>Pokemon name: {props.name}</h2>
+      <p>Weight: {props.weight} kg</p>
+      <p>Awesome: {props.awesome ? "yes" : "no"}</p>
+      <p>Terrifying: {props.terrifying ?  "yes" : "no"}</p>
+      <p>Abilities : ({props.abilities.length}) </p> 
+      <ul>
+      {props.abilities.map(abilitie=>{
+        return <li>{abilitie}</li>
+      })}
+      </ul>
+    </div>
+  );
+}
